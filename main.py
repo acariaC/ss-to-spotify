356066a512605f2936b355739f8c51d20ddaefe9
acariaC <oliverkuhn13qr@gmail.com>
2022-10-30 20:01:46 +0100

    Major changes to the code that broke everything Currently fixing it all

--- main.py
+++ main.py
@@ -6,17 +6,19 @@ import spotipy
 from pytesseract import pytesseract
 from spotipy.oauth2 import SpotifyOAuth
 
+
 def connectToSpotify():
-    print("Get a client ID & client secret from: https://developer.spotify.com/dashboard/applications")
+    # print("Get a client ID & client secret from: https://developer.spotify.com/dashboard/applications")
     os.environ['SPOTIPY_REDIRECT_URI'] = 'http://127.0.0.1:8080/'
-    os.environ['SPOTIPY_CLIENT_ID'] = input("Enter your client ID: ")
-    os.environ['SPOTIPY_CLIENT_SECRET'] = input("Enter your client secret: ")
+    # os.environ['SPOTIPY_CLIENT_ID'] = input("Enter your client ID: ")
+    # os.environ['SPOTIPY_CLIENT_SECRET'] = input("Enter your client secret: ")
+
 
 
 def usernameAuthenticator():
     try:
         print("Note that entering the wrong username will currently crash the application.")
-        return input("Enter your username: ")
+        return "lvrkhn"  # input("Enter your username: ")
     except requests.exceptions.HTTPError or spotipy.SpotifyException:  # Does not work
         print("You can not create a playlist for another user.")
         usernameAuthenticator()
@@ -31,20 +33,42 @@ token = SpotifyOAuth(scope=scope, username=username)
 spotifyObject = spotipy.Spotify(auth_manager=token)
 
 # Creating the playlist
+# playlistName = input("Enter a playlist name: ")
 
-playlistName = input("Enter a playlist name: ")
+playlistName = "ss-test"
 playlistDesc = "Created with ss-to-spotify: https://github.com/acariaC/ss-to-spotify"
-
 spotifyObject.user_playlist_create(user=username, name=playlistName, public=True, description=playlistDesc)
+
+# spotifyObject.user_playlist_create(user=username, name=playlistName, public=True, description=playlistDesc)
 listOfSongs = []
 
 # Getting the images from a directory
-folder_dir = input("Please choose your photo directory: ")
+folder_dir = "/Users/oliverkuhn/Documents/iCloud Photos/"  # input("Please choose your photo directory: ")
 img = cv2.imread(folder_dir)
+
+
+def resultHandler(i):
+    artistAndTitle = pytesseract.image_to_string(i, config=options)
+    result = re.sub(r"[^a-zA-Z0-9]+", ' ', artistAndTitle).strip()
+    if result:
+        # print(result)
+        lengthCheck = len(result.split())
+        if lengthCheck >= 3:
+            print(result)
+            song = spotifyObject.search(q=result)
+            try:
+                print(song['tracks']['items'][0]['uri'])
+                listOfSongs.append(song['tracks']['items'][0]['uri'])
+            except IndexError:
+                print("No song found.")
+    else:
+        print("Not a valid file.")
+
+# newCropped_image = img[1670: 1800, 250: 1000]
 
 for images in os.listdir(folder_dir):
     img = cv2.imread(folder_dir + images)
-    if images.endswith('.PNG') or (images.endswith('.JPEG')):
+    if images.endswith('.PNG') or (images.endswith('.jpeg')):
         dimensions = img.shape
 
         if dimensions == (2532, 1170, 3):
@@ -77,6 +101,7 @@ prePlaylist = spotifyObject.user_playlists(user=username)
 playlist = prePlaylist['items'][0]['id']
 
 # Adding songs
+print(listOfSongs)
 spotifyObject.user_playlist_add_tracks(user=username, playlist_id=playlist, tracks=listOfSongs)
 
 # cv2.imshow("Boxed", cropped_image)
