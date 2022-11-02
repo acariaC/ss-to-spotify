954db65685224a363217a5bebd443aaaa86bccca
acariaC <oliverkuhn13qr@gmail.com>
2022-11-02 14:40:55 +0100

    Making adjustments to text recognition and using one method instead of two

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 import cv2
 import requests.exceptions
 import spotipy
-from pytesseract import pytesseract
+from pytesseract import pytesseract, Output
 from spotipy.oauth2 import SpotifyOAuth
 
 
@@ -12,7 +12,6 @@ def connectToSpotify():
     os.environ['SPOTIPY_REDIRECT_URI'] = 'http://127.0.0.1:8080/'
     # os.environ['SPOTIPY_CLIENT_ID'] = input("Enter your client ID: ")
     # os.environ['SPOTIPY_CLIENT_SECRET'] = input("Enter your client secret: ")
-
 
 
 def usernameAuthenticator():
@@ -37,32 +36,14 @@ spotifyObject = spotipy.Spotify(auth_manager=token)
 
 playlistName = "ss-test"
 playlistDesc = "Created with ss-to-spotify: https://github.com/acariaC/ss-to-spotify"
-spotifyObject.user_playlist_create(user=username, name=playlistName, public=True, description=playlistDesc)
+# spotifyObject.user_playlist_create(user=username, name=playlistName, public=True, description=playlistDesc)
 
 # spotifyObject.user_playlist_create(user=username, name=playlistName, public=True, description=playlistDesc)
 listOfSongs = []
 
 # Getting the images from a directory
-folder_dir = "/Users/oliverkuhn/Documents/iCloud Photos/"  # input("Please choose your photo directory: ")
+folder_dir = "/Users/oliverkuhn/Documents/iCloud Photos/Sample/"  # input("Please choose your photo directory: ")
 img = cv2.imread(folder_dir)
-
-
-def resultHandler(i):
-    artistAndTitle = pytesseract.image_to_string(i, config=options)
-    result = re.sub(r"[^a-zA-Z0-9]+", ' ', artistAndTitle).strip()
-    if result:
-        # print(result)
-        lengthCheck = len(result.split())
-        if lengthCheck >= 3:
-            print(result)
-            song = spotifyObject.search(q=result)
-            try:
-                print(song['tracks']['items'][0]['uri'])
-                listOfSongs.append(song['tracks']['items'][0]['uri'])
-            except IndexError:
-                print("No song found.")
-    else:
-        print("Not a valid file.")
 
 # newCropped_image = img[1670: 1800, 250: 1000]
 
@@ -70,14 +51,29 @@ for images in os.listdir(folder_dir):
     img = cv2.imread(folder_dir + images)
     if images.endswith('.PNG') or (images.endswith('.jpeg')):
         dimensions = img.shape
-
-        if dimensions == (2532, 1170, 3):
+        xyRatio = float(format(img.shape[1] / img.shape[0], ".2f")) # Might be optimized with round() later
+        print(xyRatio)
+        if xyRatio == 0.46:
             # Cropping the image down to the relevant parts
-            cropped_image = img[870: 1030, 380: 1000]
+            cv2.rectangle(img, (0, 520), (0, 1659), (255, 0, 255), -1)
+            cropped_image = img[int(img.shape[0]*0.35): int(img.shape[0]*0.70), int(img.shape[1]*0.23): int(img.shape[1]*0.90)]
+
+
+            d = pytesseract.image_to_data(img, output_type=Output.DICT)
+            n_boxes = len(d['level'])
+            for i in range(n_boxes):
+                (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
+            cv2.imshow("Cropped Image", cropped_image)
+            cv2.waitKey(0)
 
             # Getting the text in the cropped image
             options = r'-c textord_min_xheight=55 -c ' \
                       r'preserve_interword_spaces=0'
+
+
 
             artistAndTitle = pytesseract.image_to_string(cropped_image, config=options)
             result = re.sub(r"[^a-zA-Z0-9]+", ' ', artistAndTitle).strip()
@@ -93,8 +89,8 @@ for images in os.listdir(folder_dir):
                     except IndexError:
                         print("No song found.")
 
-                        # cv2.imshow("Cropped Image", cropped_image)
-                        # cv2.waitKey(0)
+                        #cv2.imshow("Cropped Image", cropped_image)
+                        #cv2.waitKey(0)
 
 # Finding the new playlist
 prePlaylist = spotifyObject.user_playlists(user=username)
