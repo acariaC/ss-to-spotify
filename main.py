83df4982b91a7dbb2f23fa74749972a296e57333
acariaC <oliverkuhn13qr@gmail.com>
2022-11-02 22:32:25 +0100

    Text recognition works once again!

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import re
 import cv2
@@ -36,61 +37,44 @@ spotifyObject = spotipy.Spotify(auth_manager=token)
 
 playlistName = "ss-test"
 playlistDesc = "Created with ss-to-spotify: https://github.com/acariaC/ss-to-spotify"
-# spotifyObject.user_playlist_create(user=username, name=playlistName, public=True, description=playlistDesc)
 
 # spotifyObject.user_playlist_create(user=username, name=playlistName, public=True, description=playlistDesc)
 listOfSongs = []
 
 # Getting the images from a directory
-folder_dir = "/Users/oliverkuhn/Documents/iCloud Photos/Sample/"  # input("Please choose your photo directory: ")
+folder_dir = "/Users/oliverkuhn/Documents/iCloud Photos/"  # input("Please choose your photo directory: ")
 img = cv2.imread(folder_dir)
-
-# newCropped_image = img[1670: 1800, 250: 1000]
 
 for images in os.listdir(folder_dir):
     img = cv2.imread(folder_dir + images)
     if images.endswith('.PNG') or (images.endswith('.jpeg')):
         dimensions = img.shape
-        xyRatio = float(format(img.shape[1] / img.shape[0], ".2f")) # Might be optimized with round() later
-        print(xyRatio)
+        xyRatio = float(format(img.shape[1] / img.shape[0], ".2f"))  # Might be optimized with round() later
+
         if xyRatio == 0.46:
-            # Cropping the image down to the relevant parts
-            cv2.rectangle(img, (0, 520), (0, 1659), (255, 0, 255), -1)
-            cropped_image = img[int(img.shape[0]*0.35): int(img.shape[0]*0.70), int(img.shape[1]*0.23): int(img.shape[1]*0.90)]
+            cropped_image = img[int(img.shape[0] * 0.35): int(img.shape[0] * 0.75),
+                            int(img.shape[1] * 0.23): int(img.shape[1] * 0.90)]
 
+            # cv2.imshow("Cropped Image", cropped_image)
+            # cv2.waitKey(0)
+            artistAndTitle = (pytesseract.image_to_string(cropped_image))
 
-            d = pytesseract.image_to_data(img, output_type=Output.DICT)
-            n_boxes = len(d['level'])
-            for i in range(n_boxes):
-                (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-
-            cv2.imshow("Cropped Image", cropped_image)
-            cv2.waitKey(0)
-
-            # Getting the text in the cropped image
-            options = r'-c textord_min_xheight=55 -c ' \
-                      r'preserve_interword_spaces=0'
-
-
-
-            artistAndTitle = pytesseract.image_to_string(cropped_image, config=options)
             result = re.sub(r"[^a-zA-Z0-9]+", ' ', artistAndTitle).strip()
             if result:
-                # print(result)
                 lengthCheck = len(result.split())
-                if lengthCheck >= 3:
-                    print(result)
+                if 3 <= lengthCheck < 30:
                     song = spotifyObject.search(q=result)
                     try:
-                        print(song['tracks']['items'][0]['uri'])
+                        #print(song['tracks']['items'][0]['uri'])
+                        print('Track: ' + song['tracks']['items'][0]['name'])
+                        print('Artist: ' + song['tracks']['items'][0]['album']['artists'][0]['name'] + '\n')
+
+
+
+                        # print(json.dumps(song['tracks']['items'][0], indent=4))
                         listOfSongs.append(song['tracks']['items'][0]['uri'])
                     except IndexError:
-                        print("No song found.")
-
-                        #cv2.imshow("Cropped Image", cropped_image)
-                        #cv2.waitKey(0)
+                        print("No song found. \n")
 
 # Finding the new playlist
 prePlaylist = spotifyObject.user_playlists(user=username)
@@ -100,5 +84,4 @@ playlist = prePlaylist['items'][0]['id']
 print(listOfSongs)
 spotifyObject.user_playlist_add_tracks(user=username, playlist_id=playlist, tracks=listOfSongs)
 
-# cv2.imshow("Boxed", cropped_image)
 print("Finished.")
