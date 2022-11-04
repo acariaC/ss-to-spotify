1ed1e21212fd7ae7469da06660ba2b652a461019
acariaC <oliverkuhn13qr@gmail.com>
2022-11-04 18:18:01 +0100

    Removing width adjustments, increased height of img

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ playlistDesc = "Created with ss-to-spotify: https://github.com/acariaC/ss-to-spo
 listOfSongs = []
 
 # Getting the images from a directory
-folder_dir = "/Users/oliverkuhn/Documents/iCloud Photos/"  # input("Please choose your photo directory: ")
+folder_dir = "/Users/oliverkuhn/Documents/iCloud Photos/Sample/"  # input("Please choose your photo directory: ")
 img = cv2.imread(folder_dir)
 
 for images in os.listdir(folder_dir):
@@ -52,11 +52,13 @@ for images in os.listdir(folder_dir):
         xyRatio = float(format(img.shape[1] / img.shape[0], ".2f"))  # Might be optimized with round() later
 
         if xyRatio == 0.46:
-            cropped_image = img[int(img.shape[0] * 0.35): int(img.shape[0] * 0.75),
-                            int(img.shape[1] * 0.23): int(img.shape[1] * 0.90)]
+            # cv2.rectangle(img, (603, 1304), (603, 1304), (255, 0, 0), -1)
+            cropped_image = img[int(img.shape[0] * 0.25): int(img.shape[0] * 0.75)]
 
-            # cv2.imshow("Cropped Image", cropped_image)
-            # cv2.waitKey(0)
+            # cv2.rectangle(cropped_image, (100, 500), (100, 500), (255, 0, 0), -1)
+
+            cv2.imshow("Cropped Image", cropped_image)
+            cv2.waitKey(0)
             artistAndTitle = (pytesseract.image_to_string(cropped_image))
 
             result = re.sub(r"[^a-zA-Z0-9]+", ' ', artistAndTitle).strip()
@@ -65,13 +67,9 @@ for images in os.listdir(folder_dir):
                 if 3 <= lengthCheck < 30:
                     song = spotifyObject.search(q=result)
                     try:
-                        #print(song['tracks']['items'][0]['uri'])
                         print('Track: ' + song['tracks']['items'][0]['name'])
                         print('Artist: ' + song['tracks']['items'][0]['album']['artists'][0]['name'] + '\n')
 
-
-
-                        # print(json.dumps(song['tracks']['items'][0], indent=4))
                         listOfSongs.append(song['tracks']['items'][0]['uri'])
                     except IndexError:
                         print("No song found. \n")
